47052efbd23de243fe0fbb7b1d3d86b8136ed62d
amyagit <amit.vijay.kale@gmail.com>
2017-07-13 12:29:06 +0100

    https://github.com/amyagit/test

---
+++ testasynccall/testasynccall/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading; 
+using System.Net.Mail;
+using System.Text.RegularExpressions;
+namespace testasynccall
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IsValidEmail("gbási2003@yahoo.com");
+            methode();
+            Console.WriteLine("this methode thread call");
+            callmethode();
+           
+            Console.ReadLine();
+        }
+
+        public static bool IsValidEmail(string email)
+        {
+            bool isValidEmail = false;
+            try
+            {
+                var validEmail = new MailAddress(email);
+                isValidEmail = true;
+            }
+            catch (FormatException ex)
+            {
+                // The email has not a good format
+            }
+            return isValidEmail;
+        }
+
+
+        public static  async void methode()
+        {
+
+            await Task.Run(new Action(longRun));
+            Console.WriteLine("this methode finish");  
+
+        }
+
+        public static void longRun()
+        {
+            Thread.Sleep(10000);  
+            
+        }
+
+     public static   async void callmethode()
+        {
+
+            await Task.Run(new Action(longRun));
+            Console.WriteLine("this callmethode finish");  
+        }
+
+    }
+}
